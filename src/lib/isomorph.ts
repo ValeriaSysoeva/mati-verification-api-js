7c6f655373acdd32a25c3f7a69c8f44af5c0b18b
vedi <shubin.fedor@gmail.com>
2019-11-28 13:32:28 +0300

    Fix isomorph for browser

--- src/lib/isomorph.ts
+++ src/lib/isomorph.ts
@@ -8,13 +8,16 @@ interface Global extends NodeJS.Global {
 }
 
 const btoa: btoaType = (global as Global).btoa
-  || ((str: string) => Buffer.from(str).toString('base64'));
+  ? (global as Global).btoa.bind(undefined)
+  : ((str: string) => Buffer.from(str).toString('base64'));
 
 // eslint-disable-next-line global-require
 const FormData = (global as Global).FormData || require('form-data');
 
 // eslint-disable-next-line global-require
-const fetch: fetchType = (global as Global).fetch || require('isomorphic-fetch');
+const fetch: fetchType = (global as Global).fetch
+  ? (global as Global).fetch
+  : require('node-fetch');
 
 export default {
   btoa,
